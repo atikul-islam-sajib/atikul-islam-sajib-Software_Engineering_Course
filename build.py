b003b4999d1a43d148dbb3fd7d7d5200089f6f11
Atikul Islam Sajib <atikul.sajib@ptb.de>
2023-11-26 20:30:32 +0100

    Build management file is added

---
+++ build.py
@@ -0,0 +1,14 @@
+from pybuilder.core import use_plugin, init
+
+use_plugin('python.core')
+use_plugin('python.unittest')
+use_plugin('python.flake8')
+use_plugin('python.coverage')
+use_plugin('python.distutils')
+
+name = "iris_classifier"
+default_task = "publish"
+
+@init
+def set_properties(project):
+    project.set_property("coverage_threshold", False)
